b85f89d78fb178148a48d42b2e208d368f06ddb2
juansebvf <jsvargasf@innovaciones.co>
2021-07-06 10:57:32 -0500

    agregando y corrigiendo estilos

--- js/main.js
+++ js/main.js
@@ -1,28 +1,11 @@
 
 $(document).ready(function(){
-    const root = document.querySelector(":root");
-const container = document.querySelector(".contacto-container");
-
-function adjustContainerWidth() {
-  console.log("Resize");
-  let containerW = container.offsetWidth + 'px';
-  console.log(containerW);
-
-  root.style.setProperty("--container-width", containerW);
-};
-
-window.resize = function(){
-  adjustContainerWidth();
-};
-
-window.addEventListener("resize", adjustContainerWidth);
-window.onload = function(){
-  adjustContainerWidth();
-};
     $('.your-class').slick({
         infinite: true,
         slidesToShow: 3,
-        slidesToScroll: 3
+        slidesToScroll: 3,
+        dots:true,
+        arrows: true
     });
   });
 
