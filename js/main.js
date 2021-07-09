ea8c9b3578dc4f6a1ce1db7149e789c3cdecb3c3
juansebvf <jsvargasf@innovaciones.co>
2021-07-09 18:05:19 -0500

    Estilos responsive y detalles finales

--- js/main.js
+++ js/main.js
@@ -4,8 +4,32 @@ $(document).ready(function(){
         infinite: true,
         slidesToShow: 3,
         slidesToScroll: 1,
-        dots:false,
-        arrows: true
+        arrows: true,
+        responsive: [
+            {
+              breakpoint: 1024,
+              settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                infinite: true,
+                dots:false
+              }
+            },
+            {
+              breakpoint: 600,
+              settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+              }
+            },
+            {
+              breakpoint: 480,
+              settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+              }
+            }
+          ]
     });
   });
 
